ed1442b838ed2c4d560e286b4a018400ff88b1fc
aKaravanov <ak97@uw.edu>
2019-10-17 02:51:25 -0700

    Style and Functionality Changes

--- sortPage.js
+++ sortPage.js
@@ -12,6 +12,7 @@
   let canvas;
   let strokeWidth;
   let speed = 1;
+  let isSorting = false;
 
   let i;
   let j;
@@ -22,9 +23,22 @@
   function initializePage() {
     createCanvas();
     resetArray();
+    setUpControls();
     drawCurrentState();
+  }
 
+  function setUpControls() {
+    // Hide buttons
+    resetAllButtons();
+    document.getElementById("sortButton").style.display = "inline";
+
+    // Add listeners to buttons
     document.getElementById("sortButton").addEventListener("click", sort);
+    document.getElementById("pauseButton").addEventListener("click", pause);
+    document.getElementById("resetButton").addEventListener("click", reset);
+    document.getElementById("stepButton").addEventListener("click", step);
+
+    // Add listeners to sliders
     document.getElementById("problemSizeSlider")
       .addEventListener("input", reset);
     document.getElementById("speedSlider")
@@ -39,17 +53,49 @@
     speed = Math.round(1.0 * parseInt(event.target.value) / 10.0) + 1;
   }
 
+  function step() {
+    if (!isSorting) {
+      makeAnIteration();
+      drawCurrentState();
+    }
+  }
+
+  /**
+   * Update the speed variable
+   * @param {Event} event Trigger event
+   */
+  function pause(event) {
+      switch(event.target.innerText) {
+        case "Pause":
+          window.cancelAnimationFrame(eventId);
+          document.getElementById("stepButton").style.display = "inline";
+          event.target.innerText = "Play";
+          isSorting = false;
+          break;
+        case "Play":
+          document.getElementById("stepButton").style.display = "none";
+          eventId = window.requestAnimationFrame(draw);
+          event.target.innerText = "Pause";
+          isSorting = true;
+          break;
+        }
+  }
+
   /**
    * Resets problem to adjust with a new size
    * @param {Event} event Trigger event
    */
-  function reset(event) {
+  function reset() {
+    isSorting = false;
     window.cancelAnimationFrame(eventId);
     let ctx = canvas.getContext('2d');
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    resetArray(event.target.value);
+    resetArray();
     drawCurrentState();
+
+    resetAllButtons();
+    document.getElementById("sortButton").style.display = "inline";
   }
 
   /** Creates canvas DOM */
@@ -60,34 +106,41 @@
 
   /** Performs sorting when button is pressed */
   function sort() {
+    document.getElementById("sortButton").style.display = "none";
+    document.getElementById("pauseButton").style.display = "inline";
+    document.getElementById("resetButton").style.display = "inline";
     eventId = window.requestAnimationFrame(draw);
+    isSorting = true;
+  }
+
+  function resetAllButtons() {
+    Array.from(document.getElementsByTagName("button")).map((element) => {
+      element.style.display = "none";
+      return element;
+    });
+
+    document.getElementById("pauseButton").innerText = "Pause";
   }
 
   /** Draws sorting procedure on canvas */
   function draw() {
-    let ctx = canvas.getContext('2d');
-
-    // Reset
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     // Make n number of iterations where n is controlled by selected speed
     for (let steps = 0; steps < speed; steps++) {
-      if (workingArray[j] > workingArray[j + 1]) {
-        swap(j);
-      }
-      j += 1;
-      if (j === workingArray.length) {
-        j = 0;
-        i += 1;
-      }
+      makeAnIteration();
+      // End iterations if hit end
+      if (i === workingArray.length)
+        break;
     }
 
     // Draw current state
     drawCurrentState();
 
     // Repeat if needed
-    if (i < canvas.width) {
+    if (i < workingArray.length) {
       eventId = window.requestAnimationFrame(draw);
+    } else {
+      resetAllButtons();
+      document.getElementById("resetButton").style.display = "inline";
     }
   }
 
@@ -95,8 +148,8 @@
   function setUpCanvas() {
     canvas = document.createElement("CANVAS");
     canvas.id = "canvas";
-    canvas.width = 600;
-    canvas.height = 300;
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
     canvas.style.background = 'black';
     canvas.style.marginLeft = 'auto';
     canvas.style.marginRight = 'auto';
@@ -105,14 +158,31 @@
 
   /** Using stored array, created virtual representation of it on canvas */
   function drawCurrentState() {
-    let temp = canvas.getContext("2d");
+    let ctx = canvas.getContext('2d');
+
+    // Reset
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw all of the items
     for (let k = 0; k < workingArray.length; k++) {
-      temp.beginPath();
-      temp.lineWidth = strokeWidth;
-      temp.moveTo((k + 0.5) * strokeWidth, canvas.height);
-      temp.lineTo((k + 0.5) * strokeWidth, canvas.height - workingArray[k]);
-      temp.strokeStyle = "white";
-      temp.stroke();
+      if (j === k) {
+        ctx.fillStyle = "red";
+      } else {
+        ctx.fillStyle = "white";
+      }
+      ctx.fillRect(k*strokeWidth, canvas.height - workingArray[k],
+        strokeWidth, workingArray[k]);
+    }
+  }
+
+  function makeAnIteration() {
+    if (workingArray[j] > workingArray[j + 1]) {
+      swap(j);
+    }
+    j += 1;
+    if (j === workingArray.length) {
+      j = 0;
+      i += 1;
     }
   }
 
@@ -121,7 +191,9 @@
    * @param {String} size Reading from slider corresponding to size of the
    * problem
    */
-  function resetArray(size = 100) {
+  function resetArray() {
+    console.log(document.getElementById("problemSizeSlider"));
+    let size = document.getElementById("problemSizeSlider").value;
     workingArray = [];
     let lengthOfArray = parseInt(size) + 5;
     strokeWidth = canvas.width / lengthOfArray;
